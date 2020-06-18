261afcb39f62c6ee11346e6939c6f0fc2fa7a137
qikong233 <249414764@qq.cm>
2020-06-18 11:29:42 +0800

    init

---
+++ WeKnock-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeKnock-SwiftUI
+//
+//  Created by qikong on 2020/6/18.
+//  Copyright © 2020 qikong233. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
